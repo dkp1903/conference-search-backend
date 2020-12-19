358637809668fd17118926d59fbfedac718440d4
dkp1903 <dushyantpathak6789@gmail.com>
2020-12-19 09:29:25 +0530

    add video walkthrough

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Tasks -
     - Fetch Data
     - Remove exact duplicates
     - Remove semantic duplicates
+
+A video walkthrough can be found [here](https://www.loom.com/share/0096b5fb7d5747eeb6ae953766fd1a50)
--- services/removeSimilars.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
-const URL = `https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences`
-
-const fetchData = async() => {
-    const allData = await axios(URL)
-    console.log(allData)
-}
